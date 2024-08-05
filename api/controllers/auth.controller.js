d17f839a3064b6731601472f1a0897a215f66ba4
Deranindu Gunasekara <140329900+DeraniDU@users.noreply.github.com>
2024-08-05 20:52:58 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import User from "../models/user.model.js";
 import bcrpytjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
+
 
 export const signup = async(req, res,next) => {
     const { username, email, password } = req.body;
@@ -17,3 +20,29 @@ export const signup = async(req, res,next) => {
     }
    
 };
+
+export const signin = async(req, res,next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne ({ email });
+        if (!validUser) 
+            return next(errorHandler(404, "User not found"));
+
+        
+        const validPassword = bcrpytjs.compareSync(password, validUser.password);
+        if (!validPassword)
+            return next(errorHandler(400, "Wrong credentials"));
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: pass, ...rest } = validUser.doc;
+
+        res.cookie('access_token', token, {
+            httpOnly: true,
+        }).status(200).json(rest);
+    }
+
+    catch (error){
+        next(error);
+    }
+    }
+
+     
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { signup } from '../controllers/auth.controller.js';
+import { signin, signup } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 
 router.post('/signup',signup);  // post request for signup
-
+router.post('/signin',signin);
 export default router;  // export userRoute
