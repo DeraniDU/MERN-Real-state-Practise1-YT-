1cc25fa7b8bdcb6e9713cd844b5e8a5109492f6e
Deranindu Gunasekara <140329900+DeraniDU@users.noreply.github.com>
2024-09-27 14:22:50 +0530

    Add delete user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,43 +1,50 @@
-import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs';
+import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
 
-import bcryptjs from "bcryptjs";
-import { errorHandler } from "../utils/error.js";
-
-// Export the test function
 export const test = (req, res) => {
-    res.json({
-        message: "hello world",
-    });
+  res.json({
+    message: 'Api route is working!',
+  });
 };
 
-// Export the updateUser function
 export const updateUser = async (req, res, next) => {
-    if (req.user.id !== req.params.id) {
-        return next(errorHandler(401, 'You can only update your own account'));
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, 'You can only update your own account!'));
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
     }
 
-    try {
-        if (req.body.password) {
-            req.body.password = bcryptjs.hashSync(req.body.password, 10);
-        }
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
 
-        const updateUser = await User.findByIdAndUpdate(req.params.id, {
-            $set: {
-                username: req.body.username,
-                email: req.body.email,
-                password: req.body.password,
-                avatar: req.body.avatar,
-            }
-        }, { new: true });
+    const { password, ...rest } = updatedUser._doc;
 
-        if (!updateUser) {
-            return next(errorHandler(404, 'User not found'));
-        }
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
 
-        const { password, ...rest } = updateUser._doc;
-        res.status(200).json(rest);
-    } catch (error) {
-        console.error('Error updating user:', error);
-        next(error); // Pass the error to the global error handler
-    }
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, 'You can only delete your own account!'));
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie('access_token');
+    res.status(200).json('User has been deleted!');
+  } catch (error) {
+    next(error);
+  }
 };
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,12 @@
 import express from 'express';  // import express
-import { test, updateUser } from '../controllers/user.controller.js';  // import your controller
+import { deleteUser, test, updateUser } from '../controllers/user.controller.js';  // import your controller
 import { verifyToken } from '../utils/verifyUser.js';  // import your middleware
 const router = express.Router(); // create express router
 
 router.get('/test', test);  // get request for test
 router.post('/update/:id', verifyToken, updateUser);  // post request for update use
+router.delete('/delete/:id', verifyToken, deleteUser);  // delete request for delete user
+
+
 
 export default router;  // export the router
