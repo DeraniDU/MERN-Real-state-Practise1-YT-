6ea4db56d201d8e1bb91af81f6e151d5e27c2541
Deranindu Gunasekara <140329900+DeraniDU@users.noreply.github.com>
2024-09-28 23:50:05 +0530

    create listing functionality

--- api/index.js
+++ api/index.js
@@ -3,41 +3,39 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 dotenv.config();
 
-const app = express();  // create express app
-const PORT = process.env.PORT || 3000;  // port number
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-mongoose.connect(process.env.MONGO)
-    .then(() => {
-        console.log('DataBase connected is Successful!!!');
-    })
-    .catch(err => {
-        console.log('DataBase connection failed:', err);
-    });
+const app = express();
 
-app.use(express.json());  // use JSON middleware
+app.use(express.json());
 
-app.use(cookieParser());  // use cookie parser middleware
+app.use(cookieParser());
 
-// Define your routes after middleware
-app.use("/api/user", userRouter);
-app.use("/api/auth", authRouter);
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}!!! Horrayyy`);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
 });
 
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    console.error(err); // Log the error for debugging
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message,
-    });
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
-
