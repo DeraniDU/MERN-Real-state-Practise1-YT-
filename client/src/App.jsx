bb2cf171f6f40a53f5d2d3b6350717a4baab0b37
Deranindu Gunasekara <140329900+DeraniDU@users.noreply.github.com>
2024-11-19 23:57:06 +0530

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import SignIn from './pages/SignIn'; // Match file name case
-import SignUp from './pages/SignUp'; // Match file name case
+import SignIn from './pages/SignIn';
+import SignUp from './pages/Signup';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
@@ -9,6 +9,7 @@ import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
+import Search from './pages/Search';
 
 export default function App() {
   return (
@@ -16,9 +17,10 @@ export default function App() {
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/signin' element={<SignIn />} /> {/* Use matching case */}
-        <Route path='/signup' element={<SignUp />} /> {/* Use matching case */}
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
         <Route path='/about' element={<About />} />
+        <Route path='/search' element={<Search />} />
         <Route path='/listing/:listingId' element={<Listing />} />
 
         <Route element={<PrivateRoute />}>
